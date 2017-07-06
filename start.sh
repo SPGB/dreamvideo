c547cbe82f09c8041b0c893ec9867c857f980a70
SPGB <avonwodahs+github@gmail.com>
2017-07-06 18:47:23 -0400

    moving sh file locations

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ set -e
 # ${FFMPEG} -i /ddd/video.mp4 -f image2 /images/%08d.png
 
 # Movie to frames
-./1_movie2frames.sh avconv /ddd/video.mp4 /images/input jpg
+1_movie2frames.sh avconv /ddd/video.mp4 /images/input jpg
 
 # ipython /ddd/deepdreams.py $1 /images/*
 ipython 2_dreaming_time.py $i -i /images/input -o /images/output
@@ -17,5 +17,5 @@ ipython 2_dreaming_time.py $i -i /images/input -o /images/output
 # bash /ddd/frames2movie.sh avconv '/images' video.mp4 'png'
 # mv images_done.mp4 /images/
 
-./3_frames2movie.sh avconv /images/output /ddd/video.mp4 jpg
+3_frames2movie.sh avconv /images/output /ddd/video.mp4 jpg
 
